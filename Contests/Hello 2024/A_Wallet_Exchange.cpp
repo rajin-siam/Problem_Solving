e04ddeadb76e06b52ce8188ef77382853f5d837b
Md. Rajin Mashrur Siam <67337679+rajin-siam@users.noreply.github.com>
2024-01-10 23:31:03 +0600

    Add files via upload

---
+++ Contests/Hello 2024/A_Wallet_Exchange.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+int main()
+{
+    ll n;
+    cin>>n;
+    while(n--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        if((a+b)%2==0)cout<<"Bob\n";
+        else cout<<"Alice\n";
+
+
+    }
+    
+    
+
+}
